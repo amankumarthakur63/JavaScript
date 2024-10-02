b6b4b7d630aad69b6371d95ff9bf4908c4c4159e
amankumarthakur63 <amankumar005562@gmail.com>
2024-10-02 21:04:47 +0530

    second commit

---
+++ javaScript.js
@@ -0,0 +1,11 @@
+// First Progamme in JavaScript
+console.log("Welcome to my Blog, Friends");
+
+// Well Done! You do your first programme in javaScript
+
+// This is the single line comment 
+
+/* This
+is multi
+line commment
+*/
