b31643c318d02bc81b47a44b95233b2aa74a7be0
amankumarthakur63 <amankumar005562@gmail.com>
2024-12-03 00:21:30 +0530

    commit config

---
+++ 01_Basic/05_Strings.js
@@ -0,0 +1,37 @@
+const name = "aman"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String ('aman-ak-com')
+console.log(gameName[0])
+console.log(gameName.__proto__)
+
+console.log (gameName.length)
+console.log (gameName.toUpperCase())
+console.log (gameName.charAt(1))
+console.log (gameName.indexOf('k'))
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice (-8, 4)
+console.log(anotherString);
+
+const newStringOne = "      aman      "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://aman.com/aman%20kumar"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes ('amanji'));
+
+console.log(gameName.split ('-'));
+
+
+
