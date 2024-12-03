4e33d12efb52737b2b86b7e9af04cf6d2f813a55
amankumarthakur63 <amankumar005562@gmail.com>
2024-12-04 00:04:30 +0530

    commit config

--- 01_Basic/05_Strings.js
+++ 01_Basic/05_Strings.js
@@ -4,34 +4,49 @@ const repoCount = 50
 // console.log(name + repoCount + " value");
 
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
 
-const gameName = new String ('aman-ak-com')
+const gameName = new String('aman-ak-com')
 console.log(gameName[0])
 console.log(gameName.__proto__)
 
-console.log (gameName.length)
-console.log (gameName.toUpperCase())
-console.log (gameName.charAt(1))
-console.log (gameName.indexOf('k'))
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(1))
+console.log(gameName.indexOf('k'))
 
 const newString = gameName.substring(0, 4)
 console.log(newString);
 
-const anotherString = gameName.slice (-8, 4)
-console.log(anotherString);
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString)
 
 const newStringOne = "      aman      "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne)
+console.log(newStringOne.trim())
 
 const url = "https://aman.com/aman%20kumar"
 
 console.log(url.replace('%20', '-'))
 
-console.log(url.includes ('amanji'));
+console.log(url.includes('amanji'))
 
-console.log(gameName.split ('-'));
+console.log(gameName.split('-'))
 
 
+// Output
 
+// Hello my name is aman and my repo count is 50
+// a
+// {}
+// 11
+// AMAN-AK-COM
+// m
+// 6
+// aman
+// n
+//       aman
+// aman
+// https://aman.com/aman-kumar
+// false
+// [ 'aman', 'ak', 'com' ]
---
+++ 01_Basic/06_Nums_And_Math.js
@@ -0,0 +1,60 @@
+// const score = 400
+// console.log (score)
+
+// const balance = new Number(100)
+
+// console.log (balance)
+
+// console.log (balance.toString ().length)
+
+// console.log (balance.toFixed (2))
+
+// const otherNumber = 123.4966
+// console.log (otherNumber.toPrecision(3))
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// Output
+
+// 400
+// [Number: 100]
+// 3
+// 100.00
+// 123
+// 10,00,000
+
+// *********************Maths********************
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.6))
+console.log(Math.floor(4.6))
+console.log(Math.min(4, 5, 8, 2, 0))
+console.log(Math.max(4, 5, 8, 2, 0))
+
+// Math random value lies between 0 to 1 always
+console.log(Math.random())
+console.log((Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+// Output
+
+// Object [Math] {}
+// 4
+// 5
+// 5
+// 4
+// 0
+// 8
+// 0.7987920580472518
+// 5.491112617811142
+// 5
+// 19
+
