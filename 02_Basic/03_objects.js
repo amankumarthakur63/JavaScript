558dfad989a9743bc38a80e1cc2ffca66d4b48f1
amankumarthakur63 <amankumar005562@gmail.com>
2024-12-07 22:43:14 +0530

    commit config

--- 02_Basic/03_objects.js
+++ 02_Basic/03_objects.js
@@ -1,42 +1,12 @@
-// singleton 
-
-// object literals
-
-const mySym = Symbol("Key1")
-
-const JsUser = {
-    name: "Aman",
-    "full name": "Aman Kumar Thakur Ji",
-    [mySym]: "mykey1",
-    age: 14,
-    location: "Jalandhar",
-    email: "amanKumar@google.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
+const course = {
+    coursename: "JS in hindi",
+    price: "999",
+    courseInstructor: "hitesh chaudhary"
 }
 
-console.log(JsUser.name)
-console.log(JsUser.email)
-console.log(JsUser["email"]);
-console.log(JsUser["full name"]);
-console.log(JsUser[mySym]);
+const { courseInstructor: instructor } = course
 
-JsUser.email = "aman@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "aman@microsoft.com"
-console.log(JsUser)
-
-
-JsUser.greeting = function () {
-    console.log("Hello JsUser");
-}
-
-JsUser.greetingTwo = function () {
-    console.log(`Hello JsUser, ${this.name}`);
-}
-
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+console.log(instructor)
 
 
 // Output
@@ -55,3 +25,5 @@ console.log(JsUser.greetingTwo());
 //   undefined
 //   Hello JsUser, Aman
 //   undefined
+
+
