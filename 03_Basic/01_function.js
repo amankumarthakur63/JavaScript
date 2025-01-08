1a6744d93eb721d5063b984ed171c0edc37a087f
amankumarthakur63 <amankumar005562@gmail.com>
2025-01-08 21:53:04 +0530

    commit config

--- 03_Basic/01_function.js
+++ 03_Basic/01_function.js
@@ -3,12 +3,12 @@
 // console.log ("A")
 // console.log ("N")
 
-function saymyname() {
-    console.log("A")
-    console.log("M")
-    console.log("A")
-    console.log("N")
-}
+// function saymyname() {
+//     console.log("A")
+//     console.log("M")
+//     console.log("A")
+//     console.log("N")
+// }
 
 // saymyname()
 
@@ -16,27 +16,54 @@ function saymyname() {
 //     console.log(num1 + num2) 
 // }
 
-function addTwoNumber(num1, num2) { // parameter
+// function addTwoNumber(num1, num2) { // parameter
     // let result = num1 + num2
     // return result
     // console.log("aman");  // After return type nothing will execute
-    return num1 + num2
-}
+    // return num1 + num2
+// }
 
-const result = addTwoNumber(3, 5) // Agrument
+// const result = addTwoNumber(3, 5) // Agrument
 
 // console.log("Result : ", result)
 
-function loginUserMessage(username = "sam") {  // Default username
-    if (!username) {
-        console.log("Please enter a username");
-        return 
-    }
-    return `${username} just logged in`
-}
+// function loginUserMessage(username = "sam") {  // Default username
+//     if (!username) {
+//         console.log("Please enter a username");
+//         return 
+//     }
+//     return `${username} just logged in`
+// }
 
 // console.log(loginUserMessage("aman"))
 
-console.log(loginUserMessage("aman"));  // undefined
+// console.log(loginUserMessage("aman"));  // undefined
 
+// Rest operator
+function calculateCartPrize(val1, val2, ...num1) {
+    return num1
+}
+// console.log(calculateCartPrize(200, 400, 300, 848, 47))
+
+const user = {
+    username: "aman",
+    prices: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+// handleObject(user)
+handleObject({
+    username: "aman",
+    price: 399
+})
+
+const myNewArray = [200, 300, 400, 5000]
+
+function returnSecondValue (getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 300, 4000]));
 
