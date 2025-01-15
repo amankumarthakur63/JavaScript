98e3571628074e9d3fa612c2192d535e07d0c2b6
amankumarthakur63 <amankumar005562@gmail.com>
2025-01-15 16:03:58 +0530

    commit config

--- 03_Basic/02_scopes.js
+++ 03_Basic/02_scopes.js
@@ -1,14 +1,51 @@
-// var c = 200
+var c = 200
 let a = 300
 if (true){
 let a = 10
 const b = 20
 // c = 30
-console.log("INNER: ", a);
+// console.log("INNER: ", a);
 }
 
-console.log( a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one () {
+    const username = "aman"
+    function two() {
+        const website = "you tube"
+        console.log(username);
+    }
+    // console.log(website);
+    // two()
+}
+
+// one ()
+
+if (true) {
+    const username = "aman"
+    if (username === "aman") {
+        const website = " you tube"
+        // console.log(username + website);
+    }
+    // console.log (website)
+}
+// console.log (username)
+
+// +++++++++++++++ interesting +++++++++
+
+console.log(addone (5))
+
+function addone (num) {
+    return num + 1
+}
+
+
+addTwo (5)
+const addTwo = function (num) {
+    return num + 2
+}
+
 
 
