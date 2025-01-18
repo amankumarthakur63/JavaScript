04b5d870edadc4a088d83e53fe256ee8e29497b0
amankumarthakur63 <amankumar005562@gmail.com>
2025-01-18 23:46:41 +0530

    commit config

---
+++ 04_control_flow/one.js
@@ -0,0 +1,10 @@
+// if
+
+const isUserLoggIn = true
+const temperature = 41
+
+if (temperature < 50) {
+    console.log ("less than 50")
+} else {
+    console .log ("temperature is greater than 50")
+}
